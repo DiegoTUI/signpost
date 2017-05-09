4a23c9e07b72996a6d31114756f6cc9771cd05a6
Diego Lafuente Garcia <tufosa@gmail.com>
2017-05-09 16:03:12 +0200

    initdb finished

--- cmd/spinitdb/main.go
+++ cmd/spinitdb/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/user"
+	"runtime"
 	"time"
 
 	"flag"
@@ -20,6 +18,8 @@ import (
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	// read environment
 	var env string
 	flag.StringVar(&env, "env", env, "Environment: 'development' or 'production'")
@@ -53,15 +53,6 @@ func main() {
 
 	start := time.Now()
 
-	// read world cities
-	worldCities, err := readWorldCities(usr.HomeDir + "/resources/worldcities.txt")
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	log.Printf("number of worldcities: %d\n", len(worldCities))
-
 	// read capitals
 	capitals, err := readCapitals(usr.HomeDir + "/resources/capitals.json")
 	if err != nil {
@@ -69,25 +60,54 @@ func main() {
 		os.Exit(1)
 	}
 
-	log.Printf("number of capitals: %d\n", len(capitals))
+	log.Printf("number of capitals: %d\n", len(*capitals))
+
+	// read country codes
+	countryCodes, err := readCountryCodes(usr.HomeDir + "/resources/countryCodes.json")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	log.Printf("number of country codes: %d\n", len(*countryCodes))
+
+	// read world cities
+	worldCities := make(chan *models.WorldCity, 200)
+	go readWorldCities(usr.HomeDir+"/resources/worldcities.txt", worldCities)
 
 	// start building cities
 	var cities []models.City
 
-	for i := range worldCities {
-		capital := searchInCapitals(&capitals, &worldCities[i])
+	for worldCity := range worldCities {
+		countryCode := (*countryCodes)[worldCity.CountryCode]
 
-		if capital != nil {
-			city := models.City{
-				Name:       worldCities[i].City,
-				Country:    capital.Country,
-				Difficulty: 5,
-				IsCapital:  true,
-				Location:   models.NewGeoJSONPoint(worldCities[i].Latitude, worldCities[i].Longitude),
-			}
-
-			cities = append(cities, city)
+		if countryCode == nil {
+			continue
 		}
+
+		key := strings.ToLower(countryCode.Country) + "-" + strings.ToLower(worldCity.City)
+		capital := (*capitals)[key]
+
+		if capital == nil {
+			key = strings.ToLower(countryCode.Country) + "-" + strings.ToLower(worldCity.AccentCity)
+			capital = (*capitals)[key]
+		}
+
+		if capital == nil {
+			continue
+		}
+
+		city := models.City{
+			Name:       capital.Capital,
+			Country:    capital.Country,
+			Difficulty: 5,
+			IsCapital:  true,
+			Location:   models.NewGeoJSONPoint(worldCity.Latitude, worldCity.Longitude),
+		}
+
+		delete(*capitals, key)
+
+		cities = append(cities, city)
 	}
 
 	log.Printf("number of cities: %d\n", len(cities))
@@ -103,7 +123,7 @@ func main() {
 	log.Println("saving cities")
 	// save cities
 	for i := range cities {
-		err := cities[i].Upsert()
+		_, err := cities[i].Upsert()
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -123,63 +143,4 @@ func main() {
 
 	ellapsed := time.Since(start)
 	log.Printf("time ellapsed: %s\n", ellapsed)
-}
-
-func readCapitals(filePath string) ([]models.Capital, error) {
-	raw, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-	var capitals []models.Capital
-	json.Unmarshal(raw, &capitals)
-
-	return capitals, nil
-}
-
-func readWorldCities(filePath string) ([]models.WorldCity, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	scanner := bufio.NewScanner(file)
-
-	// skip the first line
-	scanner.Scan()
-
-	// fill in an array of WorldCities
-	var worldCities []models.WorldCity
-	for scanner.Scan() {
-		line := scanner.Text()
-		worldCity, err := models.NewWorldCity(line)
-		if err != nil {
-			log.Println("error parsing line: " + line)
-		}
-		worldCities = append(worldCities, *worldCity)
-	}
-
-	return worldCities, nil
-}
-
-func searchInWorldCities(worldCities []models.WorldCity, city string) *models.WorldCity {
-	for i := range worldCities {
-		if strings.EqualFold(city, worldCities[i].City) ||
-			strings.EqualFold(city, worldCities[i].AccentCity) {
-			return &worldCities[i]
-		}
-	}
-
-	return nil
-}
-
-func searchInCapitals(capitals *[]models.Capital, worldCity *models.WorldCity) *models.Capital {
-	for i := range *capitals {
-		if strings.EqualFold(worldCity.City, (*capitals)[i].Capital) ||
-			strings.EqualFold(worldCity.AccentCity, (*capitals)[i].Capital) {
-			result := &(*capitals)[i]
-			*capitals = append((*capitals)[:i], (*capitals)[i+1:]...)
-			return result
-		}
-	}
-
-	return nil
 }
